ef254d3e178b28a4a5b197c09f56aa59f48d69e8
Zac Hanson <zacthepk@gmail.com>
2018-12-26 21:29:28 +0000

    Building out Thor's Revenge landing page, set up routes, added fonts, updated css, i need a new computer with a mouse that works

---
+++ app.js
@@ -0,0 +1,38 @@
+var bodyParser = require("body-parser"),
+expressSanitizer = require("express-sanitizer"),
+express        = require("express"),
+app            = express();
+
+// APP CONFIG
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+
+
+
+app.get("/", function(req, res){
+   res.render("landing");  
+});
+
+app.get("/about", function(req, res){
+   res.render("");  
+});
+
+app.get("/contact", function(req, res){
+   res.render("");  
+});
+
+app.get("/blog", function(req, res){
+   res.render("blog");  
+});
+
+app.get("/Sale", function(req, res){
+   res.render("sale");  
+});
+
+
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("I do this because i love myself!");
+})
